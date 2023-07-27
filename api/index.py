174108e69f342f997292478c7eb03f1a6b52cfbc
zfgw1553 <136322400+zfgw1553@users.noreply.github.com>
2023-07-27 08:13:52 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,21 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from duckduckgo_search import DDGS
+
 app = Flask(__name__)
 
-
 @app.route('/search')
-def search():  # put application's code here
+def search():
     keywords = request.args.get('q')
-    print(request.args.get('max_results'))
-    max_results = int(request.args.get('max_results') or "6")
+    max_results = int(request.args.get('max_results') or "3")
     results = []
-    from itertools import islice
     with DDGS() as ddgs:
         ddgs_gen = ddgs.text(keywords, safesearch='Off', timelimit='y', backend="lite")
         for r in islice(ddgs_gen, max_results):
             results.append(r)
-    return results
-
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
